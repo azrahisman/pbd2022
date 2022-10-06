a3acb9378c708eb1644a8da9a7f0308db475a418
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-10-06 11:02:38 +0700

    lab 04

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path,include
-from wishlist.views import show_wishlist
-from wishlist.views import show_xml
-from wishlist.views import show_json
-from wishlist.views import show_json_by_id #customise the name of the function created
+from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id, register, login_user, logout_user
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json'), #customise the name of the function created
-    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #customise the name of the function created
+    path('json/', show_json, name='show_json'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,21 +1,32 @@
-from django.shortcuts import render
-from wishlist.models import ItemWishlist
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from wishlist.models import ItemWishlist
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 # Create your views here.
+@login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
     data_wishlist_item = ItemWishlist.objects.all()
     context = {
         'list_item': data_wishlist_item,
-        'name': 'Azra'
+        'name': 'Azra',
+        'username': request.COOKIES['username'],
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
 
 
 def show_xml(request):
     data = ItemWishlist.objects.all()
-
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
@@ -26,4 +37,38 @@ def show_json_by_id(request, id):
     data = ItemWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def register(request):
+    form = UserCreationForm()
 
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account successfully created!')
+            return redirect('wishlist:login')
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # login first
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) # create response
+            response.set_cookie('username', username)
+            response.set_cookie('last_login', str(datetime.datetime.now())) # create last_login cookie and add it to response
+            return response
+        else:
+            messages.info(request, 'Wrong Username or Password!')
+    context = {}
+    return render(request, 'login.html', context)
+
+@login_required(login_url='/wishlist/login/')
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('wishlist:login'))
+    response.delete_cookie('last_login')
+    return response
